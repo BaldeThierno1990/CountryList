1f013bd0b599fcddbd69a25ff6516e50faea67b5
BALDE Thierno <ibrahimabotore@gmail.com>
2021-02-11 14:20:09 +0100

    Initial Commit

---
+++ CountryList/CountryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListApp.swift
+//  CountryList
+//
+//  Created by LPWAM01 on 11/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
